1b7826106e7340a49e2ed8d1987d0b21347a30fa
DiGregorioC <digregorioc@outlook.com>
2019-05-15 15:11:53 -0400

    Fix game logic, and turn display
    Fixed the game logic and turn display

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -7,65 +7,121 @@
 // require('./example')
 const events = require('./app.js')
 
+
+
 $(() => {
   // your JS code goes here
-
+  let turn = 0
   let currentPlayer = 'X'
+  const changePlayerTurn = () => {
+    $('#current-turn').text(`It is ${currentPlayer}'s Turn`)
+  }
+  $('#current-turn').text(`It is ${currentPlayer}'s Turn`)
 
-  // const nextPlayer = () => {
-  //   if (currentPlayer === 'O') {
-  //     currentPlayer = 'X'
-  //   } else {
-  //     currentPlayer = 'O'
-  //   }
-  //   return currentPlayer
+  // if (turn === 2 || turn === 4 || turn === 6 || turn === 8) {
+  //   currentPlayer = 'O'
+  // } else {
+  //   currentPlayer = 'X'
   // }
+
+  if (turn === 9) {
+    console.log('game over')
+  }
+
+  const nextPlayer = () => {
+    if (currentPlayer === 'O') {
+      currentPlayer = 'X'
+    } else {
+      currentPlayer = 'O'
+    }
+    return currentPlayer
+  }
 
   let gameBoard = ['', '', '', '', '', '', '', '', '']
 
   $('.0').on('click', (event) => {
     $(event.target).text(currentPlayer)
     gameBoard[0] = currentPlayer
+    nextPlayer()
     console.log(gameBoard)
+    console.log(turn)
+    turn++
+    changePlayerTurn()
+    return turn
   })
 
   $('.1').on('click', (event) => {
     $(event.target).text(currentPlayer)
     gameBoard[1] = currentPlayer
+    turn++
+    nextPlayer()
+    console.log(turn)
+    changePlayerTurn()
+    return turn
   })
 
   $('.2').on('click', (event) => {
     $(event.target).text(currentPlayer)
     gameBoard[2] = currentPlayer
+    turn++
+    nextPlayer()
+    changePlayerTurn()
+    console.log(turn)
   })
 
   $('.3').on('click', (event) => {
     $(event.target).text(currentPlayer)
     gameBoard[3] = currentPlayer
+    turn++
+    nextPlayer()
+    changePlayerTurn()
+    console.log(turn)
   })
 
   $('.4').on('click', (event) => {
     $(event.target).text(currentPlayer)
     gameBoard[4] = currentPlayer
+    turn++
+    nextPlayer()
+    changePlayerTurn()
+    console.log(turn)
   })
 
   $('.5').on('click', (event) => {
     $(event.target).text(currentPlayer)
     gameBoard[5] = currentPlayer
+    turn++
+    nextPlayer()
+    changePlayerTurn()
+    console.log(turn)
   })
 
   $('.6').on('click', (event) => {
     $(event.target).text(currentPlayer)
     gameBoard[6] = currentPlayer
+    turn++
+    nextPlayer()
+    changePlayerTurn()
+    console.log(turn)
   })
 
   $('.7').on('click', (event) => {
     $(event.target).text(currentPlayer)
     gameBoard[7] = currentPlayer
+    turn++
+    nextPlayer()
+    changePlayerTurn()
+    console.log(turn)
   })
 
   $('.8').on('click', (event) => {
     $(event.target).text(currentPlayer)
     gameBoard[8] = currentPlayer
+    turn++
+    nextPlayer()
+    changePlayerTurn()
+    console.log(turn)
+
+    return turn
   })
 })
